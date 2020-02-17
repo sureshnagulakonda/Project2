bedb078d669d479bd580e8556ceec80559c8d8f9
root <root@ip-172-31-12-244.us-east-2.compute.internal>
2020-02-17 10:36:53 +0000

    FirstChange

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++){
+			System.out.println(“THis is our First Java Programme :::::“);
+		}
+	}
+}
