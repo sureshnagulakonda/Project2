1569b6d9a6a1930a9052cd26b020b2b419a183bd
root <root@ip-172-31-12-244.us-east-2.compute.internal>
2020-02-17 11:43:28 +0000

    Udate

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,8 @@ public class Helloworld{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0; i<10; i++){
-			System.out.println(“THis is our First Java Programme :::::“);
-		}
-	}
+			System.out.println("THis is our First Java Programme ::");
+	
+		}}
+
 }
